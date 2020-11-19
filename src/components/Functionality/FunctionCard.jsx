143b81f4184bba73e69c10c6cda34b4b9616ddb2
BilalCHERFA <cherfabilal@gmail.com>
2020-11-19 22:51:01 +0100

    premier commit

---
+++ src/components/Functionality/FunctionCard.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import {Link} from 'react-router-dom'; 
+import './FunctionCard.css';
+
+
+
+class FunctionCard extends Component {
+  render() {
+    return (
+      <div>
+        <div className="bg-image"> </div>
+        <div className = 'fonctionality'>
+          
+          <Link className = 'devinity' to="/devinity">Devinity!</Link>
+          <Link className = 'prayer' to="/prayer"> prayer </Link>
+        
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FunctionCard;
